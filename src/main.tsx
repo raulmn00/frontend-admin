72b9279a4c27cb6451cb07654ccf2d104364bfa1
raulmn00 <raul.mariaci@gmail.com>
2023-11-30 06:16:20 -0300

    feat: add tailwildcss and UI for project && set react router dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
+import "./index.css";
+import ErrorPage from "./error-page.tsx";
+import App from "./App.tsx";
+import Login from "./pages/Login.tsx";
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage/>
+    },
+    {
+        path: "/login",
+        element: <Login/>,
+        errorElement: <ErrorPage/>
+    }
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
